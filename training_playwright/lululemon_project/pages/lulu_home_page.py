e9842d5ed79cbfd81f1c86043f51ba2d59ec1573
shirabars <shirabars@gmail.com>
2026-01-21 22:05:44 +0200

    new

--- training_playwright/lululemon_project/pages/lulu_home_page.py
+++ training_playwright/lululemon_project/pages/lulu_home_page.py
@@ -1,30 +1,47 @@
-from idna.codec import search_function
+
 from playwright.sync_api import Page
 
+from playwright.sync_api import Page, expect
 
 class HomePage:
-
-    def __init__(self, page):
+    def __init__(self, page: Page):
         self.page = page
-        self.search_input = "input.search-input"
-        self.search_results = ".product-tile"
+        self.search_input = page.locator("input[data-testid='nav-desktop-search']")
+        self.search_result = page.locator(".search-results_heading__QJ__C")
+        self.no_results_title = page.locator("[class*='no-search-results_noTitleText']")
+        self.bag_icon = page.locator("[data-testid='cart-icon']")
+        self.empty_bag_title = page.locator("h1:has-text('Give your bag some love!')")
 
-    def test_search_item(self, setup_playwright):
-        page = setup_playwright
-        page.goto("https://shop.lululemon.com")
+    def close_popup(self):
+        self.page.keyboard.press("Escape")
+        return self
 
-        self.page.fill(self.search_input, setup_playwright)
-        self.page.press(self.search_input, "Enter")
+    def fill_search(self, item_name: str):
+        self.search_input.fill(item_name)
+        return self
 
-    def get_search_results(self, setup_playwright):
-        page = setup_playwright
-        page.goto("https://shop.lululemon.com")
+    def press_search_enter(self):
+        self.search_input.press("Enter")
+        return self
 
-        return self.page.query_selector_all(self.search_results)
+    def check_search_result_visible(self):
+        expect(self.search_result).to_be_visible()
+        return self
+
+    def verify_no_results_found(self):
+        expect(self.no_results_title).to_be_visible(timeout=10000)
+        return self
+
+    def click_on_bag_icon(self):
+        self.bag_icon.click()
+        return self
+
+    def verify_bag_is_empty(self):
+        expect(self.empty_bag_title).to_be_visible(timeout=7000)
+        return self
 
 
-    def count_search_results(self):
-        return len(self.get_search_results())
+
 
 
 
--- training_playwright/lululemon_project/tests/test_lulu_project.py
+++ training_playwright/lululemon_project/tests/test_lulu_project.py
@@ -1,41 +1,50 @@
-import time
-from playwright.sync_api import expect
+
 from training_playwright.lululemon_project.pages.lulu_home_page import HomePage
-
-
 
 def test_search_item(setup_playwright):
     page = setup_playwright
     page.goto("https://shop.lululemon.com/story/women")
+    home = HomePage(page)
 
-    page.keyboard.press("Escape")
-
-    # search_bar = page.locator('[name="Ntt"]')
-    search_input = page.locator("input[data-testid='nav-desktop-search']")
-    search_input.fill("jacket")
-    search_input.press("Enter")
-
-    search_result = page.locator(".search-results_heading__QJ__C")
-    expect(search_result).to_be_visible()
+    home.close_popup()
+    home.fill_search("jacket")
+    home.press_search_enter()
+    home.check_search_result_visible()
 
     print("*** test search results for jacket ***")
+
+
+def test_search_invalid_product(setup_playwright):
+    page = setup_playwright
+    page.goto("https://shop.lululemon.com/story/women")
+    home = HomePage(page)
+
+    home.close_popup()
+    home.fill_search("bitch")
+    # סליחה על השפה אבל החיפוש רק לא מקבל קללות
+    home.press_search_enter()
+    home.verify_no_results_found()
+
+    print("*** test search invalid input passed successfully ***")
+
+def test_empty_bag_message(setup_playwright):
+    page = setup_playwright
+    page.goto("https://shop.lululemon.com")
+    home = HomePage(page)
+
+    home.close_popup()
+    home.click_on_bag_icon()
+    home.verify_bag_is_empty()
+
+    print("*** test empty bag passed ***")
+
 
 def test_feedback_button(setup_playwright):
     page = setup_playwright
     page.goto("https://shop.lululemon.com/story/women")
+    page.keyboard.press("Escape")
 
 
 
 
-# def add_item_to_cart(page):
-#     LoginPage(page).go_to()
-#     LoginPage(page).login("standard_user", "secret_sauce")
-#
-#     home = HomePage(page)
-#     home.add_first_product_to_cart()
-#
-#     cart = CartPage(page)
-#     cart.open_cart()
-#
-#     assert cart.get_items_count() == 1
-# 📄 utils/test_data.py
+
